ee93c27c82210c5836181814ea8a52b6824e4b4b
jayden-n <jaydennguyen.dev@gmail.com>
2023-06-14 17:43:28 -0400

    serving static files

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const app = express();
 
 // NOTE: MIDDLEWARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello from the middleware 👋🏼');
